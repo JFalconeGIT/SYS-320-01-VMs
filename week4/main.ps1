1c0818c4229196dc81d20c4d389e6360210b4275
JFalconeGIT <jayden.falcone@mymail.champlain.edu>
2026-02-10 19:27:13 -0500

    Adding Main.ps1 and Parsing Apache Logs.ps1 Scripts

---
+++ week4/main.ps1
@@ -0,0 +1,5 @@
+﻿# . (Join-Path $PSScriptRoot Parsing_apache_specifics.ps1)
+. (Join-Path $PSScriptRoot parsing_apache_logs.ps1)
+
+ApacheLogs1
+
---
+++ week4/parsing_apache_logs.ps1
@@ -0,0 +1,23 @@
+﻿function ApacheLogs1(){
+$logsNotFormatted = Get-Content C:\xampp\apache\logs\access.log
+$tableRecords = @()
+
+for($i=0; $i -lt $logsNotFormatted.Length; $i++) {
+    $words = $logsNotFormatted[$i].Split(" ");
+    $tableRecords += [PSCustomObject]@{ "IP" = $words[0]; `
+                                        "Time" = $words[3].Trim('['); `
+                                        "Method" = $words[5].Trim('"'); `
+                                        "Page" = $words[6]; `
+                                        "Protocol" = $words[7].Trim('"'); `
+                                        "Response" = $words[8]; `
+                                        "Refrrer" = $words[10]; `
+                                        "Client" = $words[11..($words.Count - 1)] -join ' ' }}
+                                       
+return $tableRecords | Where-Object {$_.IP -match "10.*" }
+
+}
+$tableResults = ApacheLogs1
+Write-Host ($tableResults | Where-Object { $_.Page -ilike "*page2.html*" -and `
+                                           $_.Referrer -ilike "*index.html*"
+                                         }`
+        | Format-Table -Autosize | Out-String)
