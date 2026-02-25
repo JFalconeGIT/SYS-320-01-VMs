e79d1113d5478fccc87756748c27894300e7c7db
JFalconeGIT <jayden.falcone@mymail.champlain.edu>
2026-02-25 14:01:59 -0500

    Changes to be committed: 	new file:   week7/Configuration.ps1 	new file:   week7/changeConfiguration.ps1 	new file:   week7/configuration.txt 	new file:   week7/configurationMenu.ps1 	new file:   week7/readConfiguration.ps1

---
+++ week7/Configuration.ps1
@@ -0,0 +1,21 @@
+﻿$Prompt = "`n"
+$Prompt += "Select an Option:`n"
+$Prompt += "1: Show Configuration`n"
+$Prompt += "2: Change configuration`n"
+$Prompt += "3: Exit`n"
+
+$operation = $true
+
+while($operation){
+
+    Write-Host $Prompt | Out-String 
+    $choice = Read-Host
+
+    if($choice -eq 3){
+        Write-Host "Goodbye fam" | Out-String
+        exit
+        $operation = $false
+    }
+
+    elseif($choice -eq 1){
+           
---
+++ week7/readConfiguration.ps1
@@ -0,0 +1,8 @@
+﻿function readConfiguration(){
+
+    $config = Get-Content C:\Users\champuser\SYS-320-01-VMs\week7\configuration.txt
+    $configRecords = @()
+
+
+return $configFile
+}
