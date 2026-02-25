60846c1144fd3307b41ece3a8f0b5d512c740e88
JFalconeGIT <jayden.falcone@mymail.champlain.edu>
2026-02-25 11:13:51 -0500

    Changes to be committed: 	new file:   week6/Event-Logs.ps1 	new file:   week6/String-Helper.ps1 	new file:   week6/Users.ps1 	new file:   week6/main-1.ps1

---
+++ week6/String-Helper.ps1
@@ -0,0 +1,44 @@
+﻿# String-Helper
+#This script contains functions that help with String/Match/Search operations.
+
+#Functions: Get Matching Lines
+#Input:   1) Text with multiple lines  2) Keyword
+#Output:  1) Array of lines that contain the keyword
+
+function getMatchingLines($contents, $lookline){
+
+$allines = @()
+$splitted =  $contents.split([Environment]::NewLine)
+
+for($j=0; $j -lt $splitted.Count; $j++){  
+ 
+   if($splitted[$j].Length -gt 0){  
+        if($splitted[$j] -ilike $lookline){ $allines += $splitted[$j] }
+   }
+
+}
+
+return $allines
+}
+function checkPassword {
+    param(
+        [System.Security.SecureString]$securePassword
+    )
+
+    # Convert SecureString to plain text
+    $BSTR = [System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($securePassword)
+    $password = [System.Runtime.InteropServices.Marshal]::PtrToStringAuto($BSTR)
+
+    # Conditions
+    $lengthCheck  = $password.Length -ge 6
+    $letterCheck  = $password -match '[A-Za-z]'
+    $numberCheck  = $password -match '\d'
+    $specialCheck = $password -match '[^a-zA-Z\d]'
+
+    if ($lengthCheck -and $letterCheck -and $numberCheck -and $specialCheck) {
+        return $true
+    }
+    else {
+        return $false
+    }
+}
---
+++ week6/main-1.ps1
@@ -0,0 +1,157 @@
+﻿. (Join-Path $PSScriptRoot Users.ps1)
+. (Join-Path $PSScriptRoot Event-Logs.ps1)
+
+clear
+
+$Prompt = "`n"
+$Prompt += "Please choose your operation:`n"
+$Prompt += "1 - List Enabled Users`n"
+$Prompt += "2 - List Disabled Users`n"
+$Prompt += "3 - Create a User`n"
+$Prompt += "4 - Remove a User`n"
+$Prompt += "5 - Enable a User`n"
+$Prompt += "6 - Disable a User`n"
+$Prompt += "7 - Get Log-In Logs`n"
+$Prompt += "8 - Get Failed Log-In Logs`n"
+$Prompt += "9 - List At-Risk Users`n"
+$Prompt += "10 - Exit`n"
+
+$operation = $true
+
+while($operation){
+
+    
+    Write-Host $Prompt | Out-String
+    $choice = Read-Host 
+
+
+    if($choice -eq 10){
+        Write-Host "Goodbye" | Out-String
+        exit
+        $operation = $false 
+    }
+
+    elseif($choice -eq 1){
+        $enabledUsers = getEnabledUsers
+        Write-Host ($enabledUsers | Format-Table | Out-String)
+    }
+
+    elseif($choice -eq 2){
+        $notEnabledUsers = getNotEnabledUsers
+        Write-Host ($notEnabledUsers | Format-Table | Out-String)
+    }
+
+    # create user 
+     elseif($choice -eq 3){ 
+
+        $name = Read-Host -Prompt "Please enter the username for the new user"
+        $password = Read-Host -AsSecureString -Prompt "Please enter the password for the new user"
+
+        # Check if user already exists
+        if (checkUser $name) {
+            Write-Host "User already exists. Cannot create duplicate user."
+            continue
+        }
+
+        # Validate password
+        if (-not (checkPassword $password)) {
+            Write-Host "Password does not meet complexity requirements."
+            Write-Host "Minimum 6 characters, at least 1 letter, 1 number, and 1 special character."
+            continue
+        }
+
+        createAUser $name $password
+        Write-Host "User: $name is created."
+    }
+        createAUser $name $password
+
+        Write-Host "User: $name is created." | Out-String
+    }
+
+    # Remove a user
+    elseif($choice -eq 4){
+
+        $name = Read-Host -Prompt "Please enter the username for the user to be removed"
+
+        # TODO: Check the given username with the checkUser function.
+
+        removeAUser $name
+
+        Write-Host "User: $name Removed." | Out-String
+    }
+
+
+    # Enable a user
+    elseif($choice -eq 5){
+
+
+        $name = Read-Host -Prompt "Please enter the username for the user to be enabled"
+
+        # TODO: Check the given username with the checkUser function.
+
+        enableAUser $name
+
+        Write-Host "User: $name Enabled." | Out-String
+    }
+
+
+    # Disable a user
+    elseif($choice -eq 6){
+
+        $name = Read-Host -Prompt "Please enter the username for the user to be disabled"
+
+        # TODO: Check the given username with the checkUser function.
+
+        disableAUser $name
+
+        Write-Host "User: $name Disabled." | Out-String
+    }
+
+
+    elseif($choice -eq 7){
+
+        $name = Read-Host -Prompt "Please enter the username for the user logs"
+
+        # TODO: Check the given username with the checkUser function.
+
+        $userLogins = getLogInAndOffs 90
+        # TODO: Change the above line in a way that, the days 90 should be taken from the user
+
+        Write-Host ($userLogins | Where-Object { $_.User -ilike "*$name"} | Format-Table | Out-String)
+    }
+
+
+    elseif($choice -eq 8){
+
+        $name = Read-Host -Prompt "Please enter the username for the user's failed login logs"
+
+        # TODO: Check the given username with the checkUser function.
+
+        $userLogins = getFailedLogins 90
+        # TODO: Change the above line in a way that, the days 90 should be taken from the user
+
+        Write-Host ($userLogins | Where-Object { $_.User -ilike "*$name"} | Format-Table | Out-String)
+    }
+
+    elseif($choice -eq 9){
+
+        $days = Read-Host -Prompt "Enter number of days to check"
+
+        $failedLogins = getFailedLogins $days
+
+        $riskyUsers = $failedLogins |
+            Group-Object User |
+            Where-Object { $_.Count -gt 10 } |
+            Select-Object Name, Count
+
+        if ($riskyUsers.Count -eq 0) {
+            Write-Host "No at-risk users found."
+        }
+        else {
+            Write-Host "Users with more than 10 failed logins:"
+            Write-Host ($riskyUsers | Format-Table | Out-String)
+        }
+            else {
+                Write-Host "Invalid selection. Please choose a valid option from the menu."
+            }
+    }
