990a162779d340e6df1e498d6c80291b89c31e5e
Jayden Falcone <jayden.falcone@mymail.champlain.edu>
2026-04-13 21:34:20 -0400

    Refactor apache log menu script functions and options

--- week12/apacheLogMenu-1.bash
+++ week12/apacheLogMenu-1.bash
@@ -2,78 +2,73 @@
 
 logFile="/var/log/apache2/access.log.1"
 
-A
-A
-B
-B
-B
-
-
 function displayAllLogs(){
 	cat "$logFile"
 }
 
 function displayOnlyIPs(){
-        cat "$logFile" | cut -d ' ' -f 1 | sort -n | uniq -c
+	cat "$logFile" | cut -d ' ' -f 1 | sort -n | uniq -c
 }
 
-# function: displayOnlyPages:
-# like displayOnlyIPs - but only pages
+function displayOnlyPages(){
+	cat "$logFile" | cut -d ' ' -f 7 | sort | uniq -c | sort -n
+}
 
 function histogram(){
-
-	local visitsPerDay=$(cat "$logFile" | cut -d " " -f 4,1 | tr -d '['  | sort \
+	local visitsPerDay=$(cat "$logFile" | cut -d " " -f 4,1 | tr -d '[' | sort \
                               | uniq)
-	# This is for debugging, print here to see what it does to continue:
-	# echo "$visitsPerDay"
-
-        :> newtemp.txt  # what :> does is in slides
+	:> newtemp.txt
 	echo "$visitsPerDay" | while read -r line;
 	do
 		local withoutHours=$(echo "$line" | cut -d " " -f 2 \
                                      | cut -d ":" -f 1)
-		local IP=$(echo "$line" | cut -d  " " -f 1)
-          
+		local IP=$(echo "$line" | cut -d " " -f 1)
 		local newLine="$IP $withoutHours"
 		echo "$IP $withoutHours" >> newtemp.txt
-	done 
+	done
 	cat "newtemp.txt" | sort -n | uniq -c
 }
 
-# function: frequentVisitors: 
-# Only display the IPs that have more than 10 visits
-# You can either call histogram and process the results,
-# Or make a whole new function. Do not forget to separate the 
-# number and check with a condition whether it is greater than 10
-# the output should be almost identical to histogram
-# only with daily number of visits that are greater than 10 
+function frequentVisitors(){
+	# Call histogram and filter for counts greater than 10
+	histogram | while read -r line;
+	do
+		local count=$(echo "$line" | cut -d ' ' -f 1)
+		if [[ "$count" -gt 10 ]]; then
+			echo "$line"
+		fi
+	done
+}
 
-# function: suspiciousVisitors
-# Manually make a list of indicators of attack (ioc.txt)
-# filter the records with this indicators of attack
-# only display the unique count of IP addresses.  
-# Hint: there are examples in slides
-
-# Keep in mind that I have selected long way of doing things to 
-# demonstrate loops, functions, etc. If you can do things simpler,
-# it is welcomed.
+function suspiciousVisitors(){
+	# ioc.txt should contain indicators of attack, one per line
+	# Example ioc.txt contents:
+	# sqlmap
+	# nikto
+	# /etc/passwd
+	# ../
+	while read -r ioc;
+	do
+		grep "$ioc" "$logFile"
+	done < ioc.txt | cut -d ' ' -f 1 | sort | uniq -c | sort -n
+}
 
 while :
 do
 	echo "PLease select an option:"
 	echo "[1] Display all Logs"
 	echo "[2] Display only IPS"
-	# Display only pages visited
+	echo "[3] Display only Pages"
 	echo "[4] Histogram"
-	# Frequent visitors
-	# Suspicious visitors
+	echo "[5] Frequent Visitors"
+	echo "[6] Suspicious Visitors"
 	echo "[7] Quit"
 
 	read userInput
 	echo ""
 
 	if [[ "$userInput" == "7" ]]; then
-		echo "Goodbye"		
+		echo "Goodbye"
 		break
 
 	elif [[ "$userInput" == "1" ]]; then
@@ -84,15 +79,23 @@ do
 		echo "Displaying only IPS:"
 		displayOnlyIPs
 
-	# Display only pages visited
+	elif [[ "$userInput" == "3" ]]; then
+		echo "Displaying only Pages:"
+		displayOnlyPages
 
 	elif [[ "$userInput" == "4" ]]; then
 		echo "Histogram:"
 		histogram
 
-        # Display frequent visitors
-	# Display suspicious visitors
-	# Display a message, if an invalid input is given
+	elif [[ "$userInput" == "5" ]]; then
+		echo "Frequent Visitors:"
+		frequentVisitors
+
+	elif [[ "$userInput" == "6" ]]; then
+		echo "Suspicious Visitors:"
+		suspiciousVisitors
+
+	else
+		echo "Invalid option. Please select a valid menu item."
 	fi
 done
-
